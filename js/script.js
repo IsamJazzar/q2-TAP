b94d34ec147cd86d96baebf16fb9787c878f3b47
IsamJazzar <98625734+IsamJazzar@users.noreply.github.com>
2023-07-03 16:02:52 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let lis=document.querySelectorAll('li');
+let btn=document.querySelector('button');
+
+let colors=['red','green','orange','blue','teal'];
+
+
+btn.onclick=function(){
+    var twoSum = function(nums, target) {
+        for(let i = 0; i <= nums.length; i++){
+            for(let j = 0; j <= nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return [i, j]
+                }
+            }
+        }
+    };
+}
+
+
+
